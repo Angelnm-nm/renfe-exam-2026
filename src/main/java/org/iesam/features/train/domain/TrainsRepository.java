47ab5b2b9ff1db6115d2c2d938ceb10001747174
MNA <angel.martinnavas2323@gmail.com>
2026-05-14 16:58:28 +0200

    I do in correct form the pack of data and the class TrainsRepository

---
+++ src/main/java/org/iesam/features/train/domain/TrainsRepository.java
@@ -0,0 +1,11 @@
+package org.iesam.features.train.domain;
+
+import java.util.ArrayList;
+
+public interface TrainsRepository {
+    ArrayList<Train> getAll();
+    void add(Train train);
+    void delete(String id);
+    Train getId(String id);
+
+}
